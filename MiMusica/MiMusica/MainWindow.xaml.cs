4521ebdc288663723a35ab7175c65c505ccd00b9
Diana Orbe <dorbe@ucol.mx>
2019-10-18 19:14:40 -0500

    Cambio de repositorio

---
+++ MiMusica/MiMusica/MainWindow.xaml.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;//Agregamos libreria OleDB
+using System.Data; //Agregamos System.Data
+
+namespace MiMusica
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        OleDbConnection con; //Agregamos la conexión
+        DataTable dt;   //Agregamos la tabla
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Conectamos la base de datos a nuestro archivo Access
+            con = new OleDbConnection();
+            con.ConnectionString = "Provider=Microsoft.Jet.Oledb.4.0; Data Source=" + AppDomain.CurrentDomain.BaseDirectory + "\\CancionesDB.mdb";
+            MostrarDatos();
+        }
+        //Mostramos los registros de la tabla
+        private void MostrarDatos()
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "select * from Musica";
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            dt = new DataTable();
+            da.Fill(dt);
+            gvDatos.ItemsSource = dt.AsDataView();
+
+            if (dt.Rows.Count > 0)
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Hidden;
+                gvDatos.Visibility = System.Windows.Visibility.Visible;
+            }
+            else
+            {
+                lbContenido.Visibility = System.Windows.Visibility.Visible;
+                gvDatos.Visibility = System.Windows.Visibility.Hidden;
+            }
+        }
+
+        //Limpiamos todos los campos
+        private void LimpiaTodo()
+        {
+            txtId.Text = "";
+            txtCancion.Text = "";
+            txtArtista.Text = "";
+            cbGenero.SelectedIndex = 0;
+            txtAlbum.Text = "";
+            btnNuevo.Content = "Nuevo";
+            txtId.IsEnabled = true;
+        }
+        private void BtnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            OleDbCommand cmd = new OleDbCommand();
+            if (con.State != ConnectionState.Open)
+                con.Open();
+            cmd.Connection = con;
+
+            if (txtId.Text != "")
+            {
+                if (txtId.IsEnabled == true)
+                {
+                    if (cbGenero.Text != "Selecciona Género")
+                    {
+                        cmd.CommandText = "insert into Musica(Id,Cancion,Artista,Genero,Album) " +
+                            "Values(" + txtId.Text + ",'" + txtCancion.Text + "','" + txtArtista.Text + "','" + cbGenero.Text + "','" + txtAlbum.Text + "')";
+                        cmd.ExecuteNonQuery();
+                        MostrarDatos();
+                        MessageBox.Show("Canción agregada correctamente...");
+                        LimpiaTodo();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Favor de seleccionar el genero....");
+                    }
+                }
+                else
+                {
+                    cmd.CommandText = "update Musica set Artista='" + txtArtista.Text + "',Cancion='" + txtCancion.Text + "',Genero='" + cbGenero.Text + "',Album='" + txtAlbum.Text
+                        + "' where Id=" + txtId.Text;
+                    cmd.ExecuteNonQuery();
+                    MostrarDatos();
+                    MessageBox.Show("Datos de la canción Actualizados...");
+                    LimpiaTodo();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Favor de poner el ID de una canción.......");
+            }
+        }
+
+        private void BtnEditar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                txtId.Text = row["Id"].ToString();
+                txtCancion.Text = row["Cancion"].ToString();
+                txtArtista.Text = row["Artista"].ToString();
+                cbGenero.Text = row["Genero"].ToString();
+                txtAlbum.Text = row["Album"].ToString();
+                txtId.IsEnabled = false;
+                btnNuevo.Content = "Actualizar";
+            }
+            else
+            {
+                MessageBox.Show("Favor de seleccionar una canción...");
+            }
+
+        }
+
+        private void BtnEliminar_Click(object sender, RoutedEventArgs e)
+        {
+            if (gvDatos.SelectedItems.Count > 0)
+            {
+                DataRowView row = (DataRowView)gvDatos.SelectedItems[0];
+                OleDbCommand cmd = new OleDbCommand();
+                if (con.State != ConnectionState.Open)
+                    con.Open();
+                cmd.Connection = con;
+                cmd.CommandText = "Delete from Musica where Id=" + row["Id"].ToString();
+                cmd.ExecuteNonQuery();
+                MostrarDatos();
+                MessageBox.Show("Canción eliminada correctamente...");
+                LimpiaTodo();
+            }
+
+        }
+
+        private void BtnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            LimpiaTodo();
+        }
+
+        private void BtnSalir_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
+
